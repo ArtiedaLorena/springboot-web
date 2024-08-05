86d980060f324b6b9f9577289449fe420710e8a0
Lorena Artieda <76899464+ArtiedaLorena@users.noreply.github.com>
2024-08-05 11:34:29 -0300

    Se agrega vista de thymeleaf para las listas y @modelAtribute

--- src/main/java/com/lorena/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/lorena/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -3,7 +3,13 @@ package com.lorena.curso.springboot.webapp.springboot_web.controllers;
 import com.lorena.curso.springboot.webapp.springboot_web.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class UserController {
@@ -11,10 +17,28 @@ public class UserController {
 
     @GetMapping("/details")
     public String details(Model model){
-        User user = new User("Lorena", "Artieda");
+        User user = new User("Lorena", "Artieda", "artiedalorena@gmail.com");
         model.addAttribute("title","Hola mundo con Spring Boot");
         model.addAttribute("user", user);
         return "details";
+    }
+
+
+    @GetMapping("/list")
+    public String list(ModelMap model){
+        model.addAttribute("title", "Listado de Usuarios!");
+        return "list";
+
+    }
+    //Pasar datos a la vista, que sean comuneas ala mayoria de los metodos del controlador
+    @ModelAttribute("users")
+    public List<User>usersModel (){
+        return  Arrays.asList(
+                new User("Lorena", "Artieda", "artiedalorena@gmail.com"),
+                new User("Pedro", "Garcia", "pedrograrcia@gmail.com"),
+                new User("Martin", "Fernandez", "martinfernandez@gmail.com"));
+
+
     }
 
 }
--- src/main/java/com/lorena/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/lorena/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 //RestController convierte el metodo handler en un metodo rest
 @RestController
@@ -18,12 +17,29 @@ public class UserRestController {
     //@RequestMapping(path="/details, method=RequestMethod.GET")#Otra forma de escribirlo
     @GetMapping("/details")
     public UserDto details(){
-        User user = new User("Lorena", "Artieda");
+        User user = new User("Lorena", "Artieda", "artiedalorena@gmail.com");
         UserDto userDto = new UserDto();
         userDto.setUser(user);
         userDto.setTitle("Hola mundo con Springboot MVC");
 
         return userDto;
+    }
+    @GetMapping("/list")
+    public List<User>list(){
+        User user = new User("Lorena", "Artieda", "artiedalorena@gmail.com");
+        User user2 = new User("Pedro", "Garcia", "pedrograrcia@gmail.com");
+        User user3 = new User("Martin", "Fernandez", "martinfernandez@gmail.com");
+
+
+        List<User> users = Arrays.asList(user, user2, user3);
+        //List<User>users = new ArrayList<>();
+        //users.add(user);
+        //users.add(user2);
+        //users.add(user3);
+
+        return  users;
+
+
     }
 
 }
